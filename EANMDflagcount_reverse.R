9aaf9acd537252b677c1267b773e30c61c8c2e40
dontkme <hukaining@gmail.com>
2022-08-18 12:00:33 -0500

    update EANMDflagcount_reverse.R

--- EANMDflagcount_reverse.R
+++ EANMDflagcount_reverse.R
@@ -52,40 +52,40 @@ write.table(keytable2,file = keytable2outname,sep = "\t",col.names=NA)
   write.table(SEUS_Pos_table2,file=SEUS_Pos_table2outname,sep = "\t",col.names=NA)
  Framekeytable2<- table( sortedTFNMD$key,sortedTFNMD$Frame_shift_flag)
  # head(Framekeytable2)
- Framekeytable2outname <- paste(opt$Output,"Key2Frame_flag_table.txt",sep=".")
+ Framekeytable2outname <- paste(opt$Output,"Key2Frame_flag_table.txt", sep = ".")
  # print(Framekeytable2outname)
-  write.table(Framekeytable2,file=Framekeytable2outname,sep = "\t",col.names=NA)
+  write.table(Framekeytable2,file = Framekeytable2outname,sep = "\t", col.names = NA)
 # mergedfile<- merge(merge(keytable2,SEUS_Pos_table2,by="key"),Framekeytable2 ,by="key")
 # head(mergedfile)
   keytable2 <- as.data.frame.matrix(keytable2)
   SEUS_Pos_table2 <- as.data.frame.matrix(SEUS_Pos_table2)
   Framekeytable2 <- as.data.frame.matrix(Framekeytable2)
-  ma<- merge(keytable2,Framekeytable2,by=0)
-  row.names(ma)<- ma$Row.names
+  ma<- merge(keytable2,Framekeytable2, b = 0)
+  row.names(ma) <- ma$Row.names
   ma$Row.names <- NULL
-  mall <- merge(ma,SEUS_Pos_table2,by=0)
-  mallname <- paste(opt$Output,"FinalUniqNMDflag.txt",sep=".")
+  mall <- merge(ma,SEUS_Pos_table2, by = 0)
+  mallname <- paste(opt$Output, "FinalUniqNMDflag.txt", sep = ".")
   names(mall) <- make.names(names(mall)) # make.names of safe column names
   mallnrow <- nrow(mall) # count number of rows
   print(mallnrow)
   for (i in 1:mallnrow){  #### Reverse ordered 2021-07-28
-    if (mall$No_info[i]>0 && mall$No_info[i]>(mall$NMD_ex[i] + mall$NMD_in[i])){   #### Add number compare 2021-08-23
-      mall$Finalflag[i]="No_info"
-    }else if (mall[i,3]>0 && mall[i,3]>(mall$NMD_ex[i] + mall$NMD_in[i])){
+    if (!is.null(mall$No_info[i]) && mall$No_info[i]>0 && mall$No_info[i]>(mall$NMD_ex[i] + mall$NMD_in[i])){   #### Add number compare 2021-08-23
+      mall$Finalflag[i] = "No_info"
+    }else if (!is.null(mall$[i,3]) && mall[i,3] > 0 && mall[i,3] > (mall$NMD_ex[i] + mall$NMD_in[i])){
       mall$Finalflag[i]="No_NMD"
-    }else if (mall$V1.x[i]>0 && mall$V1.x[i]>(mall$NMD_ex[i] + mall$NMD_in[i]) ){
+    }else if (!is.null(mall$V1.x[i]) && mall$V1.x[i]>0 && mall$V1.x[i]>(mall$NMD_ex[i] + mall$NMD_in[i]) ){
       mall$Finalflag[i]="Not_detected"
-    }else if(mall$Same.stop_codon.Need.check[i]>0){
+    }else if(!is.null(mall$Same.stop_codon.Need.check[i]) && mall$Same.stop_codon.Need.check[i]>0){
       mall$Finalflag[i]="Same_stop_codon_Need_check" #Add need_check 2021.08.13
-    }else if(mall$No.stop_codon[i]>0){
+    }else if(!is.null(mall$No.stop_codon[i]) && mall$No.stop_codon[i]>0){
        mall$Finalflag[i]="No_stop_codon" #Add No_stop_codon 2021.08.13
-    }else if( mall$Start_codon[i]>0){
+    }else if(!is.null(mall$Start_codon[i]) &&  mall$Start_codon[i]>0){
        mall$Finalflag[i]="Start_codon" #Add Start_codon 2021.08.16
-    }else if (mall$NMD_ex[i]>0 && mall$NMD_in[i]>0){
+    }else if (!is.null(mall$NMD_ex[i]) && !is.null(mall$NMD_in[i]) && mall$NMD_ex[i]>0 && mall$NMD_in[i]>0){
       mall$Finalflag[i]="NMD_ex_in"
-    }else if(mall$NMD_ex[i] >0 && mall$NMD_in[i]==0){
+    }else if(!is.null(mall$NMD_ex[i]) && !is.null(mall$NMD_in[i]) && && mall$NMD_ex[i] >0 && mall$NMD_in[i]==0){
       mall$Finalflag[i]="NMD_ex"
-    }else if(mall$NMD_ex[i]==0 && mall$NMD_in[i]>0){
+    }else if(!is.null(mall$NMD_ex[i]) && !is.null(mall$NMD_in[i]) && mall$NMD_ex[i]==0 && mall$NMD_in[i]>0){
       mall$Finalflag[i]="NMD_in"
     # }else if(mall$NMD_ex[i]==0 && mall$NMD_in[i]==0 && mall$Start_codon[i]>0){
     }
